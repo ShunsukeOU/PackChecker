5593abc63a620bd972ef5f24ff756147a63296e8
Shunsuke <kojinyoumail4service@icloud.com>
2026-04-03 19:54:57 +0900

    Initial Commit

---
+++ PackChecker/PackCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackCheckerApp.swift
+//  PackChecker
+//
+//  Created by 平良隼涼 on 2026/04/03.
+//
+
+import SwiftUI
+
+@main
+struct PackCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
